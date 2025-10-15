54106d93a43f6c494fd48eb965cebd5b9f463d97
LUCCI ANIA LUISSE FAGELA <ljf7925@students.uc-bcf.edu.ph>
2025-10-15 15:28:50 +0800

    ready na akong magcommit hihihi

---
+++ app/src/main/java/com/nepobabies/retas/UpcycleActivity.kt
@@ -0,0 +1,20 @@
+package com.nepobabies.retas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UpcycleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fragment_upcycle)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
